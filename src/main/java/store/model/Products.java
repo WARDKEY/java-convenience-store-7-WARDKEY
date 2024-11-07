78acc00a778fe1e80d826502c6fcaa2d9235e4fe
WARDKEY <tjrmsduq@naver.com>
2024-11-07 20:24:53 +0900

    feat(Products) : 상품 기능 추가

--- src/main/java/store/model/Products.java
+++ src/main/java/store/model/Products.java
@@ -1,4 +1,15 @@
 package store.model;
 
+import java.util.List;
+
 public class Products {
+    private final List<Stock> products;
+
+    public Products(List<Stock> products) {
+        this.products = products;
+    }
+
+    public List<Stock> getProducts() {
+        return products;
+    }
 }
