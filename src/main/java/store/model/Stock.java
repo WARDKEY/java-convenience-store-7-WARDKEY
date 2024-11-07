e34149d6cfc4277fde9d94d2981b535b93af25e2
WARDKEY <tjrmsduq@naver.com>
2024-11-07 20:24:35 +0900

    feat(Stock) : 재고 기능 추가

--- src/main/java/store/model/Stock.java
+++ src/main/java/store/model/Stock.java
@@ -1,4 +1,38 @@
 package store.model;
 
 public class Stock {
+    private final String name;
+    private final String price;
+    private final String quantity;
+    private final String promotion;
+
+    public Stock(String name, String price, String quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = checkQuantity(quantity);
+        this.promotion = promotion;
+    }
+
+    private String checkQuantity(String quantity) {
+        if (quantity.equals("0")) {
+            return "재고 없음";
+        }
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
 }
